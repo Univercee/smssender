f0fd518bc993fb6a84df7f25ccacc0ce3e20482b
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-03-05 08:18:59 +0400

    refactor

--- resources/js/views/index.vue
+++ resources/js/views/index.vue
@@ -1,8 +1,14 @@
 <template>
-    <form @submit.prevent="submit" class="d-flex flex-column w-50">
-        <input type="email" name="email" v-model="email" required placeholder="E-mail">
-        <input type="password" name="password" v-model="password" required placeholder="Password">
-        <input type="submit" value="Войти">
+    <form @submit.prevent="submit">
+        <div class="d-flex flex-column">
+            <label for="">E-mail</label>
+            <input type="email" name="email" v-model="email" required placeholder="test@gmail.com">
+        </div>
+        <div class="d-flex flex-column">
+            <label for="">Пароль</label>
+            <input type="password" name="password" v-model="password" required placeholder="********">
+        </div>
+        <input type="submit" value="Войти" style="margin-top: 20px;">
     </form>
 </template>
 
@@ -28,6 +34,11 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+form{
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    gap: 1rem;
+}
 </style>
